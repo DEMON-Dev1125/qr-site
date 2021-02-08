bb1147c86cbeae36e3ed54399fc6a9305d7d0769
DEMON <729513.dev@gmail.com>
2021-02-08 03:33:09 -0800

    complete qr sire

--- include/center_content.php
+++ include/center_content.php
@@ -203,132 +203,199 @@
             </div>
             </div>
             <div class="create__card">
-            <div class="row">
-                <div class="col-md-6">
-                <p class="option__title">Size</p>
-                <select name="size" id="size">
-                    <option value="400">400</option>
-                    <option value="100">100</option>
-                    <option value="200">200</option>
-                    <option value="300">300</option>
-                </select>
-                </div>
-                <div class="col-md-6">
-                <p class="option__title">Precision</p>
-                <select name="procition" id="procition">
-                    <option value="medium">M - Medium</option>
-                    <option value="small">S - Small</option>
-                    <option value="big">B - Big</option>
-                </select>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                <p class="option__title">Background</p>
-                <div id="cp-component1" class="input-group">
-                    <div class="input-group-prepend">
-                    <span class="input-group-text input-group-addon">
-                        <i></i>
-                    </span>
-                    </div>
-                    <input type="text" value="#FFFFFF" class="form-control" />
-                </div>
-                <div class="option__footer">
-                    <label class="switch">
-                    <input type="checkbox" />
-                    <span class="slider round"></span>
-                    </label>
-                    Transparent background
-                </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                <p class="option__title">Foreground</p>
-                <div id="foreground1" class="input-group">
-                    <div class="input-group-prepend">
-                    <span class="input-group-text input-group-addon">
-                        <i></i>
-                    </span>
-                    </div>
-                    <input type="text" value="#000000" class="form-control" />
-                </div>
-                <div class="option__footer">
-                    <label class="switch">
-                    <input type="checkbox" checked />
-                    <span class="slider round"></span>
-                    </label>
-                    Gradient
-                </div>
-                </div>
-                <div class="col-md-6">
-                <p class="option__title">Second color</p>
-                <div id="secondcolor1" class="input-group">
-                    <div class="input-group-prepend">
-                    <span class="input-group-text input-group-addon">
-                        <i></i>
-                    </span>
-                    </div>
-                    <input type="text" value="#8900D5" class="form-control" />
-                </div>
-                <div class="option__footer">
-                    <label class="switch">
-                    <input type="checkbox" />
-                    <span class="slider round"></span>
-                    </label>
-                    Radial
-                </div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                <p class="option__title">Marker border</p>
-                <div id="markerborder1" class="input-group">
-                    <div class="input-group-prepend">
-                    <span class="input-group-text input-group-addon">
-                        <i></i>
-                    </span>
-                    </div>
-                    <input type="text" value="#000000" class="form-control" />
-                </div>
-                <div class="option__footer">
-                    <label class="switch">
-                    <input type="checkbox" checked />
-                    <span class="slider round"></span>
-                    </label>
-                    Custom marker color
-                </div>
-                </div>
-                <div class="col-md-6">
-                <p class="option__title">Marker center</p>
-                <div id="markercenter1" class="input-group">
-                    <div class="input-group-prepend">
-                    <span class="input-group-text input-group-addon">
-                        <i></i>
-                    </span>
-                    </div>
-                    <input type="text" value="#000000" class="form-control" />
-                </div>
-                </div>
-            </div>
-
-            <div class="create__card__footer">
                 <div class="row">
-                <div class="col-6 col-sm-6 col-lg-12 col-xl-6 download">
-                    <a href="" class="download">
-                    <img src="<?php echo $relative; ?>svg/buttons/down_two.svg" alt="" />
-                    </a>
+                    <div class="col-sm-6">
+                        <label><?php echo qrcdr()->getString(
+                            'size'
+                        ); ?></label>
+                        <select name="size" class="custom-select qrcode-size-selector">
+                        <?php for ($i = 8; $i <= 32; $i += 4) {
+                            $value = $i * 25;
+                            echo '<option value="' .
+                                $i .
+                                '" ' .
+                                ($matrixPointSize == $i
+                                    ? 'selected'
+                                    : '') .
+                                '>' .
+                                $value .
+                                '</option>';
+                        } ?>
+                        </select>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <label><?php echo qrcdr()->getString(
+                            'error_correction_level'
+                        ); ?></label>
+                        <select name="level" class="custom-select">
+                            <option value="L" <?php if (
+                                $errorCorrectionLevel == 'L'
+                            ) {
+                                echo 'selected';
+                            } ?>>
+                                <?php echo qrcdr()->getString(
+                                    'precision_l'
+                                ); ?>
+                            </option>
+                            <option value="M" <?php if (
+                                $errorCorrectionLevel == 'M'
+                            ) {
+                                echo 'selected';
+                            } ?>>
+                                <?php echo qrcdr()->getString(
+                                    'precision_m'
+                                ); ?>
+                            </option>
+                            <option value="Q" <?php if (
+                                $errorCorrectionLevel == 'Q'
+                            ) {
+                                echo 'selected';
+                            } ?>>
+                                <?php echo qrcdr()->getString(
+                                    'precision_q'
+                                ); ?>
+                            </option>
+                            <option value="H" <?php if (
+                                $errorCorrectionLevel == 'H'
+                            ) {
+                                echo 'selected';
+                            } ?>>
+                                <?php echo qrcdr()->getString(
+                                    'precision_h'
+                                ); ?>
+                            </option>
+                        </select>
+                    </div>
                 </div>
-                <div class="col-6 col-sm-6 col-lg-12 col-xl-6 vector">
-                    <a href="" class="vector">
-                    <img src="<?php echo $relative; ?>svg/buttons/vector_two.svg" alt="" />
-                    </a>
+
+                <div class="row">
+                    <div class="col-sm-12 mb-2 custom-background">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <label><?php echo qrcdr()->getString(
+                                    'background'
+                                ); ?></label>
+                                <div class="collapse show" id="collapse-background">
+                                    <div class="input-group qrcolorpicker colorpickerback">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text colorpicker-input-addon"><i></i></span>
+                                        </div>
+                                        <input type="text" class="form-control" data-format="hex" value="<?php echo $stringbackcolor; ?>" name="backcolor">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="custom-control custom-switch mt-2">
+                        <input type="checkbox" class="custom-control-input collapse-control-reversed" id="trans-bg" name="transparent" data-target="#collapse-background">
+                        <label class="custom-control-label" for="trans-bg"><?php echo qrcdr()->getString(
+                            'transparent_background'
+                        ); ?></label>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-12">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <label><?php echo qrcdr()->getString(
+                                    'foreground'
+                                ); ?></label>
+                                <div class="input-group qrcolorpicker mb-2">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text colorpicker-input-addon"><i></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" data-format="hex"
+                                    value="<?php echo $stringfrontcolor; ?>" name="frontcolor">
+                                </div>
+                                <div class="form-group">
+                                    <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input collapse-control" id="gradient-bg" data-target="#collapse-gradient" name="gradient">
+                                    <label class="custom-control-label" for="gradient-bg"><?php echo qrcdr()->getString(
+                                        'gradient'
+                                    ); ?></label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="collapse" id="collapse-gradient">
+                                    <label><?php echo qrcdr()->getString(
+                                        'second_color'
+                                    ); ?></label>
+                                    <div class="input-group qrcolorpicker mb-2" id="collapse-gradient">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text colorpicker-input-addon"><i></i></span>
+                                        </div>
+                                        <input type="text" class="form-control qrcolorpicker_bg" data-format="hex" value="#8900D5" name="gradient_color">
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-switch">
+                                        <input type="checkbox" class="custom-control-input" id="radial-gradient" name="radial">
+                                        <label class="custom-control-label" for="radial-gradient"><?php echo qrcdr()->getString(
+                                            'radial'
+                                        ); ?></label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+                <div class="row">
+                    <div class="col-12 mb-2">
+                        <div class="row collapse collapse-markers-bg">
+                            <div class="col-sm-6 mt-2">
+                                <label><?php echo qrcdr()->getString(
+                                    'marker_outline'
+                                ); ?></label>
+                                <div class="input-group qrcolorpicker">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text colorpicker-input-addon"><i><div class="colorpicker-minisquare"></div></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" data-format="hex" value="<?php echo $stringfrontcolor; ?>" name="marker_out_color">
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 mt-2">
+                                <label><?php echo qrcdr()->getString(
+                                    'marker_center'
+                                ); ?></label>
+                                <div class="input-group qrcolorpicker">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text colorpicker-input-addon"><i></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" data-format="hex" value="<?php echo $stringfrontcolor; ?>" name="marker_in_color">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12 mt-2">
+                                <div class="custom-control custom-switch">
+                                <input type="checkbox" class="custom-control-input collapse-control" id="markers-bg" data-target=".collapse-markers-bg" name="markers_color">
+                                <label class="custom-control-label" for="markers-bg"><?php echo qrcdr()->getString(
+                                    'custom_markers_colors'
+                                ); ?></label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+
+                <div class="create__card__footer">
+                    <div class="row">
+                    <div class="col-6 col-sm-6 col-lg-12 col-xl-6 download">
+                        <a href="" class="download">
+                        <img src="<?php echo $relative; ?>svg/buttons/down_two.svg" alt="" />
+                        </a>
+                    </div>
+                    <div class="col-6 col-sm-6 col-lg-12 col-xl-6 vector">
+                        <a href="" class="vector">
+                        <img src="<?php echo $relative; ?>svg/buttons/vector_two.svg" alt="" />
+                        </a>
+                    </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="col-lg-2">
